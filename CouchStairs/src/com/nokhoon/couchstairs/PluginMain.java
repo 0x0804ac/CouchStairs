076742efb35bdd045cdbc60e8b12883a3e6b11d3
nokho <nokho@210.90.143.81>
2025-01-17 21:30:54 +0900

    Updated to "1.21"

--- CouchStairs/src/com/nokhoon/couchstairs/PluginMain.java
+++ CouchStairs/src/com/nokhoon/couchstairs/PluginMain.java
@@ -15,11 +15,11 @@ import org.bukkit.entity.AbstractArrow.PickupStatus;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDismountEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.util.Vector;
-import org.spigotmc.event.entity.EntityDismountEvent;
 
 public class PluginMain extends JavaPlugin implements Listener {
 	private BukkitTask task = null;
